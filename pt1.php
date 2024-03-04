3eef5e8091c2aca778fcc8d26cda798b7ba4b3b8
Hibatul Akmal <57566175+akmalhibatul@users.noreply.github.com>
2024-03-04 13:45:42 +0700

    Tugas Pertemuan 1
    tugas Pertemuan 1 pemrograman web 2

---
+++ pt1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 1 - Hibatul Akmal</title>
+</head>
+
+<body>
+    <?php
+    print "<center><b><font size=14 color='red'>BIODATA</font></b></center>";
+    print "<pre>";
+    print "<font size=12 color='blue'>";
+    print "                                 Nama :<b>Hibatul Akmal</b> <br>";
+    print "                                 Email :akmalhibatul1@gmail.com <br>";
+    print "                                 Jurusan :Teknik Informatika <br>";
+    print "                                 Hoby :Jalan jalan <br>";
+    print "</pre>";
+    ?>
+
+    <hr>
+
+    <?php
+    $kesanPertama = "Kesan pertama kali belajar PHP adalah Kagum si ya! Saya terkesan dengan kemampuan PHP dalam memproses data, membuat halaman dinamis, dan mempermudah pengembangan web.";
+
+    echo "<h3>Selamat Datang di Dunia PHP!</h3>";
+    echo "<p>$kesanPertama</p>";
+    ?>
+</body>
+
+</html>
